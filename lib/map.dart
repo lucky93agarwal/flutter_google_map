7c70fc8aeb415c3391594c29c532aed6e4fb4ce2
GPSLAB776 <140515359+GPSLAB776@users.noreply.github.com>
2023-09-11 15:15:55 +0530

    current location find and show marker

--- lib/map.dart
+++ lib/map.dart
@@ -5,6 +5,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
@@ -25,19 +26,54 @@ class MapSampleState extends State<MapSample> {
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+  // on below line we have created the list of markers
+  add(){
+    const Marker marker = Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(20.42796133580664, 75.885749655962),
+        infoWindow: InfoWindow(
+          title: 'My Position',
+        )
+    );
+    setState(() {
+      const MarkerId markerId = MarkerId("markerIdVal");
+      // adding a new marker to map
+      markers[markerId] = marker;
+    });
+  }
+  // created method for getting user current location
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission().then((value){
+    }).onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR"+error.toString());
+    });
 
+    return await Geolocator.getCurrentPosition();
+
+  }
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    add();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(markers.values),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -51,7 +87,33 @@ class MapSampleState extends State<MapSample> {
   }
 
   Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    getUserCurrentLocation().then((value) async {
+      print(value.latitude.toString() +" "+value.longitude.toString());
+
+      // marker added for current users location
+       Marker marker =  Marker(
+        markerId:const MarkerId("2"),
+        position: LatLng(value.latitude, value.longitude),
+        infoWindow:const InfoWindow(
+          title: 'My Current Location',
+        ),
+      );
+
+
+      // specified current users location
+      CameraPosition cameraPosition = CameraPosition(
+        target: LatLng(value.latitude, value.longitude),
+        zoom: 14,
+      );
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+      setState(() {
+        const MarkerId markerId = MarkerId("markerIdVal1");
+        // adding a new marker to map
+        markers[markerId] = marker;
+      });
+    });
+    // final GoogleMapController controller = await _controller.future;
+    // await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
